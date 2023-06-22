2b0bb332345d579e55b420acd5863cd86c8da1a4
Rachata Thananchai <120377188+Meaww2@users.noreply.github.com>
2023-06-22 10:49:28 +0700

    something

--- src/components/InitTheme.ts
+++ src/components/InitTheme.ts
@@ -1,43 +1,23 @@
-// import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
-// import { deepPurple } from '@mui/material/colors'
-// new code add dark theme
-import { ThemeProvider, createTheme } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+import { deepPurple } from '@mui/material/colors'
 
-const darkTheme = createTheme({
-    palette: {
-        mode: 'dark',
+const theme = extendTheme({
+    colorSchemes: {
+        light: {
+            palette: {
+                primary: {
+                    main: deepPurple[800],
+                },
+            },
+        },
+        dark: {
+            palette: {
+                primary: {
+                    main: deepPurple[100],
+                },
+            },
+        },
     },
 })
 
-function App() {
-    return (
-        <ThemeProvider theme={darkTheme}>
-            <CssBaseline />
-            <main>This app is using the dark mode</main>
-        </ThemeProvider>
-    )
-}
-
-export default App
-
-// const theme = extendTheme({
-//     colorSchemes: {
-//         light: {
-//             palette: {
-//                 primary: {
-//                     main: deepPurple[800],
-//                 },
-//             },
-//         },
-//         dark: {
-//             palette: {
-//                 primary: {
-//                     main: deepPurple[100],
-//                 },
-//             },
-//         },
-//     },
-// })
-
-// export default theme
+export default theme
