b07aea5ec94cc091a0ac35666ab4ec258d904927
Chaipat J <work.chaipat@gmail.com>
2023-06-24 00:05:49 +0700

    update pre-build

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
-import ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />,
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        {
-            name: 'singleHMR',
-            handleHotUpdate({ modules }) {
-                modules.map((m) => {
-                    m.importedModules = new Set()
-                    m.importers = new Set()
-                })
+  plugins: [
+    react(),
+    {
+      name: "singleHMR",
+      handleHotUpdate({ modules }) {
+        modules.forEach((m) => {
+          m.importedModules = new Set();
+          m.importers = new Set();
+        });
 
-                return modules
-            },
-        },
-    ],
-})
+        return modules;
+      }
+    }
+  ],
+  build: {
+    outDir: "public",
+    emptyOutDir: true
+  }
+});
